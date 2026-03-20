f9a619eb484799c61f9f009e83be750123e00d85
Spring Thanh <nastuthanh2@gmail.com>
2026-03-21 01:03:15 +0700

    update manager

--- lib/screens/manager/manager_inventory_screen.dart
+++ lib/screens/manager/manager_inventory_screen.dart
@@ -1,3 +1,4 @@
+// src/screens/manager/manager_inventory_screen.dart
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../core/constants/app_colors.dart';
